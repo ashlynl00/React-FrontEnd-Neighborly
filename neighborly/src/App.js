6674ea099c546cfe1acf01e6e1159fc96a1da905
Ashlyn Lanaville <a14079@MacBook-Air.local>
2022-03-29 14:20:31 -0700

    added nav routes

--- neighborly/src/App.js
+++ neighborly/src/App.js
@@ -1,26 +1,35 @@
 //import logo from './logo.svg';
 import './App.css';
 import ItemContainer from './ItemContainer/ItemContainer';
-import logo from '../src/icons8-marker-n-50.png';
+import Nav from './NavComponents/nav';
+import About from './NavComponents/about';
+import Contact from './NavComponents/contact';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App" alt="Logo">
-      <nav>
-        <img src={logo}></img>
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Contact</li>
-        </ul>
-      </nav>
-      <main>
-        <h1>Welcome to Neighborly!</h1>
-        <h2>Find your neighborhood community!</h2>
-        <ItemContainer></ItemContainer>
-      </main>
-    </div>
+    <Router>
+      <div className="App" alt="Logo">
+        <Nav></Nav>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/about" element={<About />}></Route>
+          <Route path="/contact" element={<Contact />}></Route>
+        </Routes>
+        
+        
+      </div>
+    </Router>
   );
-}
+};
+const Home = () => {
+  return (
+    <main>
+          <h1>Welcome to Neighborly!</h1>
+          <h2>Find your neighborhood community!</h2>
+          <ItemContainer></ItemContainer>
+    </main>
+  );
+};
 
 export default App;
---
+++ neighborly/src/NavComponents/nav.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import './style.css';
+import logo from '../icons8-marker-n-50.png';
+import {Link} from 'react-router-dom';
+
+const Nav = () => {
+    return (
+        <nav>
+          <img src={logo}></img>
+          <ul>
+            <Link to='/'>
+              <li>Home</li>
+            </Link>
+            <Link to='/about'>
+              <li>About</li>
+            </Link>
+            <Link to='/contact'>
+              <li>Contact</li>
+            </Link>
+          </ul>
+        </nav>
+    );
+};
+
+export default Nav;
